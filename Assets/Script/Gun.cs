984bee0ebd819cbedabf5353eca1c6a59e624f07
KIM-SUNGSOO <133596829+KIM-SUNGSOO@users.noreply.github.com>
2026-04-02 13:35:13 +0900

    저장소 변경(zip파일 다운로드 -> github 커밋저장소)

---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+
+
+public class Gun : MonoBehaviour
+{
+    public Transform firePoint;
+    private LineRenderer bulletLineEffect;
+    private AudioSource gunAudioPlayer;
+
+    public LayerMask targetMask;
+    public float fireRate = 0.2f;
+    private float lastFireTime;
+    public ParticleSystem gunParticle;
+
+    private void Awake()
+    {
+        gunAudioPlayer = GetComponent<AudioSource>();
+        bulletLineEffect = firePoint.GetComponent<LineRenderer>();
+
+        bulletLineEffect.positionCount = 2;
+        bulletLineEffect.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+    {
+        if (Time.time - lastFireTime > fireRate)
+        {
+            lastFireTime = Time.time;
+            Shot();
+        }
+    }
+        
+    }
+
+    public void Shot()
+    {
+        Ray ray = new Ray(firePoint.position, firePoint.forward);
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out hit, targetMask))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.origin + ray.direction * 100f;
+        }
+
+        
+        Vector3 direction = (targetPoint - firePoint.position).normalized;
+        Vector3 endPoint = firePoint.position + direction * 100f;
+
+        if (hit.collider != null)
+        {
+            endPoint = targetPoint;
+        }
+
+        StartCoroutine(CoshotEffect(endPoint));
+    }
+    private IEnumerator CoshotEffect(Vector3 hitPosition)
+    {
+
+        gunParticle.Play();
+        
+
+        
+
+        bulletLineEffect.SetPosition(0, firePoint.position);
+        bulletLineEffect.SetPosition(1, hitPosition);
+        bulletLineEffect.enabled = true;
+        yield return new WaitForSeconds(0.03f);
+
+        bulletLineEffect.enabled = false;
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 movement;
+    private Animator playerAnimator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); //A D 좌우
+        float v = Input.GetAxis("Vertical"); //W S 상하
+
+        movement = new Vector3(h, 0f , v); //Y축은 고정 필요
+
+        bool isMoving = Mathf.Abs(h) > 0.1f || Mathf.Abs(v) > 0.1f;
+        playerAnimator.SetBool("Moving", isMoving);
+
+        RotateToMouse();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void RotateToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 lookPoint = hit.point;
+
+            // Y값 고정 (기울어지는 거 방지)
+            lookPoint.y = transform.position.y;
+
+            transform.LookAt(lookPoint);
+        }
+    }
+
+}
